cac1872f8521b597709117b3b7f4a42393358b28
Nick Stockton <phoenix76728@gmail.com>
2021-08-20 16:01:14 -0400

    third commit

--- SelectionStatementsExercise/Program.cs
+++ SelectionStatementsExercise/Program.cs
@@ -6,7 +6,7 @@ namespace SelectionStatementsExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
 
             var r = new Random();
             var faveNum = r.Next(1, 1000);
@@ -27,33 +27,7 @@ namespace SelectionStatementsExercise
                 Console.WriteLine("Nevermind");
             }
 
-            Console.WriteLine("What is your favorit subject?");
-            Console.WriteLine("1: Math 2: Science 3: History 4: English 5: Art ");
-            string use2 = Console.ReadLine();
-                //int.Parse(Console.ReadLine());
-
-
-            switch (use2)
-            {
-                case "math":
-                    Console.WriteLine("Math is cool");
-                    break;
-                case "science":
-                    Console.WriteLine("Science is cool");
-                    break;
-                case "history":
-                    Console.WriteLine("History is cool");
-                    break;
-                case "english":
-                    Console.WriteLine("English is cool");
-                    break;
-                case "art":
-                    Console.WriteLine("Art is cool");
-                    break;
-                default:
-                    Console.WriteLine("Thats not a subject choice or you spelt it wrong. (All lower case)");
-                    break;
-            }
+          
 
 
         }
